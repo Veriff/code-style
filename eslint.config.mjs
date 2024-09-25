de2781749f05823cce3dbcef9384fee267b8d26a
Bogdan Bashev <bogdan.bashev@veriff.net>
2024-09-25 11:54:37 +0300

    chore: fix publish secrets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-underscore-dangle */
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
-export default [...compat.extends("@veriff/eslint-config/base-javascript")];
+export default [...compat.extends('@veriff/eslint-config/base-javascript')];
